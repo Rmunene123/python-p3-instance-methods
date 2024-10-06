691b41bdb13ec2efbdfa6f945ba574c7cc8cfc9b
Rmunene123 <rosewanmun@gmail.com>
2024-10-06 17:59:40 +0300

    added instance method

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,18 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
 
     #Instance method definition
     pass
+
+# Testing the Dog class
+if __name__ == "__main__":
+    fido = Dog()
+    fido.bark() 
+    fido.sit()   
+
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,17 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
 
     #Instance method definition
     pass
+
+# Testing the Person class
+if __name__ == "__main__":
+    john = Person()
+    john.talk()  
+    john.walk()  
